a0f9ad869f6dad6882af3f3aee37a56a4fdb6b1d
Renan Veronez Drechsler <renanvz@outlook.com>
2020-12-16 16:42:22 -0300

    Models

--- src/database/migrations/1608141528584-CreateTransactions.ts
+++ src/database/migrations/1608141528584-CreateTransactions.ts
@@ -34,7 +34,7 @@ export default class CreateTransactions1608141528584
             default: 'now()',
           },
           {
-            name: 'update_at',
+            name: 'updated_at',
             type: 'timestamp',
             default: 'now()',
           },
--- src/database/migrations/1608144528284-CreateCategories.ts
+++ src/database/migrations/1608144528284-CreateCategories.ts
@@ -24,7 +24,7 @@ export default class CreateCategories1608144528284
             default: 'now()',
           },
           {
-            name: 'update_at',
+            name: 'updated_at',
             type: 'timestamp',
             default: 'now()',
           },
