3e11a78300764eda508e45dfa40d8b4f1728aac8
Renan Veronez Drechsler <renanvz@outlook.com>
2020-12-17 11:16:00 -0300

    Delete Transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, TransactionRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    // Buscar do banco de dados
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionsRepository.findOne(id);
+
+    // Não existe?
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    // Existe?
+    await transactionsRepository.remove(transaction);
   }
 }
 
